3f1831f3cf22f3c192068d21a80e616c1226ac2a
wall0159 <a.wall.cos@gmail.com>
2013-09-26 12:11:56 +0930

    fleshing out functions

--- filter.py
+++ filter.py
@@ -1,5 +1,20 @@
+import biopython
 
+# functions to perform moving average filtering of the phred scores and return indices at which to trim
 
-# function to perform moving average filtering of the phred scores and return indices at which to trim
+# use biopython for this
+def convert_phred_scores_to_numbers(phred_scores):
+    return numeric_quality_score
+    
+# return a moving average filtered set of phred scores
+def filter_on_quality(numeric_quality_score, window_width):
+    # 
+    filtered_numeric_quality_score = list([])
+    return filtered_numeric_quality_score 
 
-def filter_on_quality(phred_scores, window_width):
+# return indexes at which to trim the sequence
+def trim_on_quality(filtered_numeric_quality_score, sequence, threshold):
+    # trim the quality score, and the sequence based on the threshold quality
+    
+    # return a new sequence object that has been trimmed
+    
