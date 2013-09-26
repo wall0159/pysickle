d4c3d8b262164c72fe502ec02124c76509cfb561
wall0159 <a.wall.cos@gmail.com>
2013-09-26 14:47:18 +0930

    remove tabs and replace with spaces

--- test/test_pysickle.py
+++ test/test_pysickle.py
@@ -1,7 +1,5 @@
 # test the main program
 
-def quality_filter_errors_on_nonnumeric_input():
-    numeric_quality_score = list([1, 2, 'A'])
-    window_width = 20
-    assert not filter_on_quality(numeric_quality_score, window_width)
+def no_file_specified():
+    assert not pysickle
 
