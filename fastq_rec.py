39e3796a57c2b0527f0640ff8c0ccfb1d2f7957d
twobeers75 <johntoubia@bigpond.com>
2013-09-26 14:06:24 +0930

    added fastq function

--- fastq_rec.py
+++ fastq_rec.py
@@ -1,18 +1,23 @@
-import biopython
+from Bio import SeqIO
 from scipy import pandas as pd
 
 class fastq_rec:
     def __init__:
     
-#    def fastq(self,file_in):
-#        '''method to parse the fastq file'''
-        
-    def get_read(self):
-        '''load one read from the fastq file'''
+    def fastq(self, filename):
+        '''load the fastq file and pass all the information with quality scores converted to Phred Values'''
+		# hard coded to take in only illumina scores but can be extended to work with others
+		for record in SeqIO.parse(filename, "fastq-illumina"):
+			self.id = record.id
+			self.seq = record.seq
+			self.phred = record.letter_annotations["phred_quality"]
+			calculate_trim_adresses(self)
+			write_to_output(self)
     
     def phred_id(self):
         '''determine the encoding of the phred score, and populate the dictionary
         guessing phred type may return AmbiguousPhredError'''
+		
         
     def calculate_trim_adresses(self):
         '''return the addresses at the 5' and 3' ends at which to trim the sequence'''
