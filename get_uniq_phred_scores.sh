d6cd30790c69aa8cbdda5ff7780df05a505d5bd4
wall0159 <a.wall.cos@gmail.com>
2013-09-26 11:42:14 +0930

    initial commit

---
+++ get_uniq_phred_scores.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+# we want the line between lines starting with '+' and lines starting with '@', return a list of uniq characters from those lines. Will need to be sensitive to the last line in the file, which will have no following '@'
---
+++ pysickle.py
@@ -0,0 +1,27 @@
+class pysickle:
+    def __init__:
+    
+    def fastq(self,file_in):
+        '''method to parse the fastq file'''
+        
+    def get_read(self):
+        '''load one read from the fastq file'''
+    
+    def phred_id(self):
+        '''determine the encoding of the phred score, and populate the dictionary
+        guessing phred type may return AmbiguousPhredError'''
+        
+    def calculate_trim_adresses(self):
+        '''return the addresses at the 5' and 3' ends at which to trim the sequence'''
+        # output
+        self.5_addr
+        self.3_addr
+        
+    def trim(self):
+        '''use the calculated addresses to trim the fastq file'''
+    
+    
+    
+    
+    
+	
